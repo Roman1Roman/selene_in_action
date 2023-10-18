9dfa7cabfb05509e22ec02daddf5005dc5502386
= <=>
2023-10-18 16:45:44 +0300

    fixing mid level steps

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,7 +1,4 @@
-import os
-from model.registration_page import RegistrationPage
-from selene import browser, be, have
-
+from demoqa_tests.pages.registration_page import RegistrationPage
 
 reg_page = RegistrationPage()
 
