df2e78a01bc929098f932f26bf63bc413df29030
Roman0Roman <127503924+Roman0Roman@users.noreply.github.com>
2023-10-26 15:33:30 +0300

    prereq to jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,39 @@
 import pytest
-from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene.support.shared import browser
+from selene import Browser, Config
+from selenium.webdriver.remote.webdriver import WebDriver
 
+from utils import attach
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='function')
 def browser_manage():
-    browser.config.base_url = 'https://demoqa.com'
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
     browser.config.window_width = 1100
     browser.config.window_height = 2300
+    browser.config.base_url = 'https://demoqa.com'
 
-    yield
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,26 +1,31 @@
 from demoqa_tests.pages.registration_page import RegistrationPage
-
+import allure
 reg_page = RegistrationPage()
 
+@allure.title('Filling registration form')
+@allure.tag('Frontend')
+def test_filling_form(browser_manage):
+    browser = browser_manage
+    with allure.step('Opening browser page'):
+        reg_page.open_page()
 
-def test_filling_form():
-    reg_page.open_page()
+    with allure.step('Filling registration form'):
+        reg_page.first_name_fill('Roman')
+        reg_page.second_name_fill('Bazaleev')
+        reg_page.email_fill('bazaleev.roma@ya.ru')
+        reg_page.gender_fill('Male')
+        reg_page.phone_fill('8800555353')
+        reg_page.birth_fill(year=2001, month=10, day=21)
+        reg_page.subjects_fill('computer science', 'physics')
+        reg_page.hobbies_fill(sports='sports', reading='reading')
+        reg_page.download_pic(file_name='1.jpg')
+        reg_page.current_address_fill('Pushkina Kolotushkina 1337')
+        reg_page.state_fill('haryana')
+        reg_page.city_fill('karnal')
+        reg_page.submit()
 
-    reg_page.first_name_fill('Roman')
-    reg_page.second_name_fill('Bazaleev')
-    reg_page.email_fill('bazaleev.roma@ya.ru')
-    reg_page.gender_fill('Male')
-    reg_page.phone_fill('8800555353')
-    reg_page.birth_fill(year=2001, month=10, day=21)
-    reg_page.subjects_fill('computer science', 'physics')
-    reg_page.hobbies_fill(sports='sports', reading='reading')
-    reg_page.download_pic(file_name='1.jpg')
-    reg_page.current_address_fill('Pushkina Kolotushkina 1337')
-    reg_page.state_fill('haryana')
-    reg_page.city_fill('karnal')
-    reg_page.submit()
-
-    reg_page.should_have_words('Roman Bazaleev', 'bazaleev.roma@ya.ru',
-                               'Male', '8800555353', 'Computer Science, Physics',
-                               '21 November,2001', '1.jpg', 'Pushkina Kolotushkina 1337',
-                               'Haryana Karnal', 'Sports, Reading')
+    with allure.step('Checking filled results'):
+        reg_page.should_have_words('Roman Bazaleev', 'bazaleev.roma@ya.ru',
+                                   'Male', '8800555353', 'Computer Science, Physics',
+                                   '21 November,2001', '1.jpg', 'Pushkina Kolotushkina 1337',
+                                   'Haryana Karnal', 'Sports, Reading')
